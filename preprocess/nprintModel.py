31dd0026456c037218f5584d5c822caaa86164e2
jaber daneshamooz <jaber.daneshamooz@gmail.com>
2024-02-09 18:19:05 +0000

    Code for preprocessing npt files resulted from nprint_script and making dataset of flowpairs

--- preprocess/nprintModel.py
+++ preprocess/nprintModel.py
@@ -1,4 +1,9 @@
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report
-from sklearn.metrics import roc_auc_score
+# from sklearn.model_selection import train_test_split
+# from sklearn.ensemble import RandomForestClassifier
+# from sklearn.metrics import classification_report
+# from sklearn.metrics import roc_auc_score
+
+
+
+if __name__ == "__main__":
+    pass
