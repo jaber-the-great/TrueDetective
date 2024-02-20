c4a9b9a2901adf82ae38d86499b2c02cd5fe7bff
jaber-the-great <jaber@ucsb.edu>
2024-02-20 06:48:57 +0000

    Totally random choise of users and flows, different from previous method, run in parallel for several datasets

--- preprocess/Representaion_pairs.py
+++ preprocess/Representaion_pairs.py
@@ -11,11 +11,14 @@ import ipaddress
 import statistics
 import numpy as np
 
+
 def userFlowStats(userFlows):
     # Number of flows per user
     # Number of total users
     Lengths = []
     for key in userFlows:
+        if len(userFlows[key]) < 2:
+            continue
         Lengths.append(len(userFlows[key]))
         if not is_valid_ip(key):
             print("error")
@@ -41,7 +44,119 @@ def userFlowStats(userFlows):
     #     json.dump(freq,jsonfile)
 
 
-def combineSameUserFlows(userFlowFile):
+def datasetPairGenerate(userFlows, outputFile, numberOfUsers,  maxFlows):
+    # Random sample: selection without replacement, but random choice is with replacement
+    print(f'Generating pairs for {outputFile}')
+    combinedList = {}
+    pairsPerClass = int(maxFlows*(maxFlows+1)//2)
+    cnt= 0
+
+
+
+    sampledKeys = random.sample(list(userFlows.keys()), numberOfUsers)
+    datasetUsers = {key: userFlows[key] for key in sampledKeys}
+    userList =  list(datasetUsers.keys())
+    weightList= [len(value) for value in datasetUsers.values()]
+
+
+
+    for user , flows in datasetUsers.items():
+        try:
+            pairs = []
+            for i in range(pairsPerClass):
+                pair = random.sample(flows, 2)
+                pair[0]=   user + '/'  + pair[0].split('/')[-1]
+                pair[1] =   user + '/' + pair[1].split('/')[-1]
+                pairs.append(pair + [1])
+
+
+            for i in range(pairsPerClass):
+                otherUser = random.choices(userList)[0]
+                while otherUser == user:
+                    otherUser = random.choices(userList)[0]
+
+                pair = [random.choice(flows), random.choice(userFlows[otherUser]), 0]
+                pair[0] = user + '/' + pair[0].split('/')[-1]
+                pair[1] =  otherUser + '/' + pair[1].split('/')[-1]
+                pairs.append(pair)
+            combinedList[user] = pairs
+        except:
+            print(f'Error for user {user}')
+    with open(f'{outputFile}' , "w") as jsonfile:
+        json.dump(combinedList,jsonfile)
+
+def groupOfDatasetGenerator(userFlowsFile, outputDir, minFlows, maxFlows):
+    arg_list = []
+    file = open(userFlowsFile)
+    userFlows = json.load(file)
+    usersToRemove = []
+    for user , flows in userFlows.items():
+        if len(flows) < minFlows:
+            usersToRemove.append(user)
+    for user in usersToRemove:
+        del userFlows[user]
+
+    for i in range(1, 50):
+        numberOfUsers = random.randint(100, 2000)
+        outputFile = f'{outputDir}/{i}_{numberOfUsers}.json'
+        arg = (userFlows,outputFile,numberOfUsers , maxFlows)
+        arg_list.append(arg)
+
+    #print(arg_list)
+    _paralell_process(datasetPairGenerate,arg_list)
+
+def readAndMergePairFiles( userPairsFile, OutputFile):
+    dataset = []
+    cols = []
+    file = open(userPairsFile)
+    userPairs = json.load(file)
+
+    cnt = 0
+    for user , pairs in userPairs.items():
+        cnt += 1
+        print(cnt)
+        for pair in pairs:
+            try:
+
+                firstFlow = pd.read_csv(f'/mnt/md0/jaber/groupedCIC/{pair[0]}')
+                secFlow = pd.read_csv(f'/mnt/md0/jaber/groupedCIC/{pair[1]}')
+                firstHeader = pd.read_csv(f'/mnt/md0/jaber/groupedHeader/{pair[0].replace("_Flow.csv", ".csv")}', header=None)
+                secondHeader=  pd.read_csv(f'/mnt/md0/jaber/groupedHeader/{pair[1].replace("_Flow.csv", ".csv")}', header=None)
+                label = pair[2]
+                joined = firstFlow.values.tolist()[0] + firstHeader.values.tolist()[0] + secFlow.values.tolist()[0] + secondHeader.values.tolist()[0]+ [label]
+                dataset.append(joined)
+                if cols == []:
+                    FirstCols = list(firstFlow.columns) + [ str(i) for i in range (len(firstHeader.columns))]
+
+                    SecCols = list(secFlow.columns)
+                    SecCols = ["x" + s for s in SecCols]
+                    for i in range(len(secondHeader.columns)):
+                        SecCols.append(f'x{str(i)}')
+
+                    cols = FirstCols + SecCols   + ['label']
+
+            except Exception as e:
+                print(user)
+                print(e)
+                # print(pairs)
+
+        df = pd.DataFrame(dataset , columns=cols)
+        df.to_csv(OutputFile + ".json", index = False)
+
+def parallelReadAndMergePairs(inputDir, outputDir):
+
+    argList = []
+    for subdir, dirs, files in  os.walk(inputDir):
+        for file in files:
+            if file.endswith(".json"):
+                name = file.split('.')[0]
+                arg = (inputDir + file,outputDir + name)
+                argList.append(arg)
+    print(argList)
+    _paralell_process(readAndMergePairFiles, argList)
+
+
+def combineUserFlows(userFlowFile):
     combinedList = {}
     userList = list(userFlows.keys())
     weightList= [len(value) for value in userFlows.values()]
@@ -106,8 +221,6 @@ def feedPairs(inputFile):
 
     print(arg_list)
     _paralell_process(createDatasetFromFlows, arg_list)
-    
-
 
 def _paralell_process(func, input_args, cores=0):
     if cores == 0:
@@ -171,7 +284,7 @@ if __name__ == "__main__":
     # file = open("/home/jaber/userGroups/UserFlows_s2f1.json")
     # userFlows = json.load(file)
     # userFlowStats(userFlows)
-    # combineSameUserFlows(userFlows)
+    # combineUserFlows(userFlows)
     # file = open("CombinedPairs6Flows.json")
     # flowPairs = json.load(file)
     # createDatasetFromFlows(flowPairs)
@@ -184,8 +297,13 @@ if __name__ == "__main__":
     # print(sum(lengths))
 
     # MergeUserGroups('/home/jaber/userGroups')
-    file = open('/home/jaber/userGroups/AllUsers.json')
-    userFlows = json.load(file)
-    userFlowStats(userFlows)
+    # file = open('/home/jaber/userGroups/AllUsers.json')
+    # userFlows = json.load(file)
+    #userFlowStats(userFlows)
+    #datasetPairGenerate(userFlows, 'temp', 1000, 5,30)
+    #groupOfDatasetGenerator('/home/jaber/userGroups/AllUsers.json', '/mnt/md0/jaber/pairsDatasets',5 , 30)
+    # readAndMergePairFiles('/mnt/md0/jaber/pairsDatasets/49_292.json', '/mnt/md0/jaber/datasets/first.pkl')
+    parallelReadAndMergePairs("/mnt/md0/jaber/pairsDatasets/", "/mnt/md0/jaber/datasets/")
+
 
 
