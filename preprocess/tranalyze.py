c2fe4a07adb63ad35b8c7182bbb67a4bd9f19693
jaber-the-great <jaber@ucsb.edu>
2024-02-27 03:28:52 +0000

    Using tranalzer to get flow stats per user

---
+++ preprocess/tranalyze.py
@@ -0,0 +1,33 @@
+from multiprocessing import Pool
+import os
+from subprocess import call
+import subprocess
+
+
+def iterateAndRunTranlyzer(input_dir,outputDir):
+    cnt = 0
+
+    for subdir, dirs, files in os.walk(input_dir):
+        for file in files:
+            if file.endswith(".pcap"):
+                user = subdir.split('/')[-1]
+                command = f'tranalyzer -r {subdir}/{file} -w {outputDir}/'
+                print(command)
+               # call(command)
+                subprocess.run('t2 -h')
+                if cnt > 2:
+                    break
+                # Todo: create the output direcotry --> is it needed or it would automatically create
+                # TODO: run tranalyzer code here
+
+
+
+
+def _paralell_process(func, input_args, cores=0):
+    if cores == 0:
+        cores = os.cpu_count()
+    with Pool(cores) as p:
+        return p.starmap(func, input_args)
+
+if __name__ == "__main__":
+    iterateAndRunTranlyzer('/mnt/md0/jaber/groupedPcap','/mnt/md0/jaber/groupedTranalyze')
