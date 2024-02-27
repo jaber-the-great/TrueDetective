c9ae3ef7b7f14c61edd46f632434848f2caa455a
jaber-the-great <jaber@ucsb.edu>
2024-02-27 20:27:46 +0000

    reading and merging pair files, 0 padding for pcaps with less than 5 packets

--- preprocess/nprintPreprocess.py
+++ preprocess/nprintPreprocess.py
@@ -74,106 +74,71 @@ def createUsersDictionary(input_file, output_dir):
         json.dump(users, fp)
 
 
-
-def userFlowStats(userFlows):
-    # Number of flows per user
-    # Number of total users
-    Lengths = []
-    for key in userFlows:
-        Lengths.append(len(userFlows[key]))
-    Lengths.sort()
-    print(Lengths)
-    # Calculates how many users have n numbre of flows
-    freq = Counter(Lengths)
-    for item , frequency in freq.items():
-        print(f"{item}: {frequency} times")
-    with open("/mnt/md0/jaber/npt/NumberofFlowsPerUser.txt", "w") as file:
-        for item in Lengths:
-            file.write(f'{item},')
-
-    with open("/mnt/md0/jaber/npt/FreqOfNumFlowsPerUsers.json", "w") as jsonfile:
-        json.dump(freq,jsonfile)
-
-def combineSameUserFlows(userFlowsFile):
-    file = open(userFlowsFile)
-    userFlows = json.load(file)
-
-    combinedList = {}
-    userList = list(userFlows.keys())
-    weightList= [len(value) for value in userFlows.values()]
-    cnt = 0
-    # Skip the users with less than 6 and more than 300 flows
-    # Then creating the same amount of combination with other users which are randomly chose
-    # IMPORTANT: Be careful about the location of the files (it is before batching them together but not need to change it at this stage)
-    for user, flows in userFlows.items():
-        cnt+=1
-        print(cnt)
-
-        try:
-            if len(flows) < 3:
-                continue
-
-            if len(flows) <= 65:
-                selected_flows = flows
-            else:
-                selected_flows = random.sample(flows, 65)
-            pairs = list(combinations(selected_flows, 2))
-            # Labeling pairs as 1
-            pairs = [(p[0], p[1], 1) for p in pairs]
-            #print(pairs)
-
-            # We use length of pairs because we want to have the same amount of records with 0 label
-            for i in range(len(pairs)):
-                ## Random choise based on the weight of each user, the weight is number of flows associated with each user
-                otherUser = random.choices(userList, weights=weightList, k=1 )[0]
-                # Making shure about incorrect possible incorrect labelling
-                while otherUser == user :
-                    otherUser = random.choice(userList)
-                newPair = (random.choice(selected_flows), random.choice(userFlows[otherUser]), 0)
-                pairs.append(newPair)
-            combinedList[user] = pairs
-        except:
-            print(f'error for user {user}')
-
-
-
-
-    with open("/mnt/md0/jaber/npt/nptCombine65Flows.json", "w") as jsonfile:
-        json.dump(combinedList,jsonfile)
-
-def createDatasetFromFlows(flowPairs , nptFile ):
-    index = 0
+def readAndMergePairFiles( userPairsFile, OutputFile):
     dataset = []
     cols = []
-    cnt = 0
-    userFlows = pd.read_csv(nptFile)
-    file = open(flowPairs)
-    flowPairs = json.load(file)
+    file = open(userPairsFile)
+    userPairs = json.load(file)
 
-    for user, pairs in flowPairs.items():
-        cnt+=1
+    cnt = 0
+    for user , pairs in userPairs.items():
+        cnt += 1
         print(cnt)
-        if cnt % 10 == 0:
-            df = pd.DataFrame(dataset, columns=cols)
-            df.to_pickle(f'/mnt/md0/jaber/npt/nptdata/{index}.pkl')
-            dataset = []
-            index +=1
+
         for pair in pairs:
             try:
+                firstflowName = pair[0].split('/')[-1].replace(".pcap_Flow.csv", ".npt")
+                secFlowName = pair[1].split('/')[-1].replace(".pcap_Flow.csv", ".npt")
+                firstFlow = pd.read_csv(f'/mnt/md0/jaber/nprintPerDataset/5_1000.json_NoRepetiti/{firstflowName}')
+                secFlow = pd.read_csv(f'/mnt/md0/jaber/nprintPerDataset/5_1000.json_NoRepetiti/{secFlowName}')
 
-                firstFlow = userFlows.iloc[pair[0]]
-                secFlow = userFlows.iloc[pair[1]]
-                label = pair[2]
-                joined = firstFlow.values.tolist() + secFlow.values.tolist() + [label]
+                if cols == []:
+                    FirstCols = list(firstFlow.columns)
+                    SecCols = list(secFlow.columns)
+                    SecCols = ["x" + s for s in SecCols]
+                    # Multiply by 5 for number of packets
+                    cols = FirstCols*5 + ['User Label'] + SecCols*5 + ['xUser Label']  + ['label']
+
+                # if len(firstFlow) >= 5 and len(secFlow) >=5:
+                #     firstFlow = pd.concat([firstFlow.iloc[i] for i in range(5)],axis =0)
+                #     secFlow = pd.concat([secFlow.iloc[i] for i in range(5)], axis=0)
+                # else:
+                #     continue
+                    # creeat first flow and second flow from the first 5 rows of the npt file if there is less than
+                    # 5 rows in the npt file, pad the rows with 0s
+
+                # If less than 5 packets in the flow, use zero padding
+                firstFlow = pd.concat([firstFlow.iloc[i] if i < len(firstFlow) else pd.Series([0]*len(firstFlow.columns)) for i in range(5)],axis =0)
+                secFlow = pd.concat([secFlow.iloc[i] if i < len(secFlow) else pd.Series([0]*len(secFlow.columns)) for i in range(5)], axis=0)
+                src =firstFlow['src_ip'][0]
+                dst = firstFlow.iloc[131:163].tolist()
+                src = checkIP(src,dst)
+                firstFlow['User Label'] = src
+
+
+                src =secFlow['src_ip'][0]
+                dst = secFlow.iloc[131:163].tolist()
+                src = checkIP(src,dst)
+                secFlow['User Label'] = src
+
+                pairlabel = pair[2]
+                joined = firstFlow.values.tolist()  + secFlow.values.tolist()  + [pairlabel]
+
                 dataset.append(joined)
-                if not cols:
-                    cols = firstFlow.index.tolist() + secFlow.index.tolist() + ['label']
-            except:
-                print(user)
-                #print(pairs)
 
-    df = pd.DataFrame(dataset, columns=cols)
-    df.to_pickle(f'/mnt/md0/jaber/dataset/{index}.pkl')
+
+            except Exception as e:
+                print(user)
+                print(e)
+
+    df = pd.DataFrame(dataset , columns=cols)
+    df.to_csv(OutputFile + ".csv", index = False)
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
@@ -185,5 +150,8 @@ if __name__ == "__main__":
     # userFlows = json.load(file)
     # userFlowStats(userFlows)
     # combineSameUserFlows("/mnt/md0/jaber/npt/NprintUsersFlows.json")
-    createDatasetFromFlows("/mnt/md0/jaber/npt/nptCombine65Flows.json" ,'/mnt/md0/jaber/npt/nprint_data.csv')
+    # createDatasetFromFlows("/mnt/md0/jaber/npt/nptCombine65Flows.json" ,'/mnt/md0/jaber/npt/nprint_data.csv')
+    # file = open('/mnt/md0/jaber/pairsDatasets/1_10.json')
+    # UserPairs = json.load(file)
+    readAndMergePairFiles('/mnt/md0/jaber/pairsDatasets/5_1000.json_NoRepetition', '/mnt/md0/jaber/mergedNprint/5_1000_NORepeat')
 
